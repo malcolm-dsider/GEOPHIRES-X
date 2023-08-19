cd539195f0eeac25e809ce8d9b2505059d3451a7
Malcolm Ross <malcolmgti@gmail.com>
2023-08-19 18:21:03 -0500

    making more changes for AAPG Training classe

---
+++ pip_install.bat
@@ -0,0 +1,15 @@
+pip install numpy
+pip install numpy-financial
+pip install pint
+pip install forex_python
+pip install jsons
+pip install json
+pip install pprint
+pip install mpmath
+pip install deepdiff
+pip install functools
+pip install mysql.connector
+pip install hashlib
+pip install h5py
+pip install scipy
+pip install itertools
